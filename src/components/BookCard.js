0e7d16010c37f502957d51f77be73c00ee76da1c
Sojin Lee <77518932+SJ-Lee33@users.noreply.github.com>
2021-12-02 12:15:14 +0900

    centralize bookcards

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,12 +1,11 @@
 import {
-  Wrap,
   Box,
   Image,
   Text,
   Center,
   Stack,
-  Button,
   Link,
+  Container,
 } from "@chakra-ui/react";
 
 const BookCard = ({
@@ -18,41 +17,44 @@ const BookCard = ({
   publishedDate,
 }) => {
   return (
-    <Link
-      href={`http://play.google.com/store/books/details?id=${id}`}
-      isExternal
-      m="0.5rem"
-      display="inline-block"
-      _hover={{
-        background: "white",
-        color: "red.500",
-        boxShadow: "xl",
-        p: "0.5rem",
-        rounded: "md",
-        bg: "white",
-      }}
-    >
-      <Center borderRadius="lg" w="25rem" m="1rem" bg="lightgrey">
-        <Box>
-          <Image h="100%" m="0.5rem" src={thumbnail} alt="thumnail"></Image>
-        </Box>
-        <Box display="inline-block" w="15rem" h="100%">
-          <Stack
-            direction="col"
-            display="block"
-            bg="white"
-            p="1rem"
-            m="0.7rem"
-            boxShadow="md"
-          >
-            <Text fontSize="xl">『{title}』</Text>
-            <Text fontSize="m">{authors}</Text>
-            <Text fontSize="sm">{publisher}</Text>
-            <Text fontSize="sm">{publishedDate}</Text>
-          </Stack>
-        </Box>
-      </Center>
-    </Link>
+    <Container maxW="xl" centerContent>
+      <Link
+        href={`http://play.google.com/store/books/details?id=${id}`}
+        isExternal
+        m="0.5rem"
+        display="inline-block"
+        textAlign="center"
+        _hover={{
+          background: "white",
+          color: "red.500",
+          boxShadow: "xl",
+          p: "0.5rem",
+          rounded: "md",
+          bg: "white",
+        }}
+      >
+        <Center borderRadius="lg" w="25rem" m="1rem" bg="lightgrey">
+          <Box>
+            <Image h="100%" m="0.5rem" src={thumbnail} alt="thumnail"></Image>
+          </Box>
+          <Box display="inline-block" w="15rem" h="100%">
+            <Stack
+              direction="col"
+              display="block"
+              bg="white"
+              p="1rem"
+              m="0.7rem"
+              boxShadow="md"
+            >
+              <Text fontSize="xl">『{title}』</Text>
+              <Text fontSize="m">{authors}</Text>
+              <Text fontSize="sm">{publisher}</Text>
+              <Text fontSize="sm">{publishedDate}</Text>
+            </Stack>
+          </Box>
+        </Center>
+      </Link>
+    </Container>
   );
 };
 
