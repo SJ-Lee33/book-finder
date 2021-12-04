3953246ba9ebff88fd885fdca9cc309850fb7f39
Sojin Lee <77518932+SJ-Lee33@users.noreply.github.com>
2021-12-04 15:07:49 +0900

    refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -6,37 +6,47 @@ import BookCard from "./BookCard";
 export default function App() {
   const [query, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [cards, setCards] = useState([]);
+  const [error, setError] = useState(false);
+  const [cards, setCards] = useState(null);
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`https://www.googleapis.com/books/v1/volumes?q=${query})`)
-      .then((res) => {
-        if (res.data.items.length > 0) {
-          setCards(res.data.items);
-          setLoading(false);
-        }
-      })
-      .catch((error) => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(false);
+      try {
+        const res = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query})`
+        );
+        setCards(res.data.items);
+        setLoading(false);
+      } catch (error) {
         setError(true);
-      });
-  }, [query]);
-
-  const mainPage = () => {
-    const PressEnter = (e) => {
-      if (e.key == "Enter") {
-        e.preventDefault();
-        search(e);
       }
     };
+    fetchData();
+  }, [query]);
 
-    const search = (e) => {
-      setSearchQuery(() => e.target.value);
-    };
+  const pressEnter = (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      search(e);
+    }
+  };
 
-    return (
+  const search = (e) => {
+    setSearchQuery(() => e.target.value);
+  };
+
+  if (loading) {
+    <Text fontSize="4xl">Loading...</Text>;
+  }
+
+  if (error || !cards) {
+    <Text fontSize="4xl">Error!</Text>;
+  }
+
+  return (
+    <div>
       <FormControl display="flex" w="30rem" alignItems="center" m="2rem 0">
         <Image src="/images/searching.png" mr="1rem" boxSize="2.3rem" />
         <Input
@@ -46,46 +56,31 @@ export default function App() {
           bg="white"
           value={query}
           onChange={search}
-          onKeyDown={PressEnter}
+          onKeyDown={pressEnter}
           placeholder="제목, 작가, 출판사를 입력해 보세요."
         />
       </FormControl>
-    );
-  };
-
-  const handleCards = () => {
-    const items = cards.map((item, i) => {
-      let thumbnail = "";
-      if (item.volumeInfo.imageLinks) {
-        thumbnail = item.volumeInfo.imageLinks.thumbnail;
-      } else {
-        thumbnail = "/images/noimage.jpg";
-      }
-      return (
-        <div key={item.id}>
-          <BookCard
-            id={item.id}
-            thumbnail={thumbnail}
-            title={item.volumeInfo.title}
-            authors={item.volumeInfo.authors}
-            publisher={item.volumeInfo.publisher}
-            publishedDate={item.volumeInfo.publishedDate}
-            isEbook={item.saleInfo.isEbook}
-          />
-        </div>
-      );
-    });
-    return (
-      <div>
-        <div>{items}</div>
-      </div>
-    );
-  };
-
-  return (
-    <div>
-      {mainPage()}
-      {handleCards()}
+      {cards?.map((item, i) => {
+        let thumbnail = "";
+        if (item.volumeInfo.imageLinks) {
+          thumbnail = item.volumeInfo.imageLinks.thumbnail;
+        } else {
+          thumbnail = "/images/noimage.jpg";
+        }
+        return (
+          <div key={item.id}>
+            <BookCard
+              id={item.id}
+              thumbnail={thumbnail}
+              title={item.volumeInfo.title}
+              authors={item.volumeInfo.authors}
+              publisher={item.volumeInfo.publisher}
+              publishedDate={item.volumeInfo.publishedDate}
+              isEbook={item.saleInfo.isEbook}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
